45397298ecca809e225cf8dac6e5a6089c568bfa
wsb <2267638690@qq.com>
2019-09-18 14:50:48 +0800

    Change-Id: I0000000000000000000000000000000000000000 本地修改、提交、上传
    Change-Id: I79bc0b21e2d615f43be9057f2e12cd95d8cf82a2

--- 测试/src/Swing/TestJFrame.java
+++ 测试/src/Swing/TestJFrame.java
@@ -1,13 +1,13 @@
 package Swing;
 
 import javax.swing.JFrame;
-/*GUI 创建一个空白窗口，后来测试git，修改窗口大小300->500;之后本地接着修改为900*/
+/*GUI 创建一个空白窗口，后来测试git，修改窗口大小300->500;之后本地接着修改为800*/
 public class TestJFrame {
 	public static void main(String[] args) {
 		JFrame f=new JFrame();
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//f.setSize(300,300);
-		f.setSize(900,900);
+		f.setSize(800,800);
 		f.setVisible(true);
 	}
 }
