4fd5ba33e0924ae3135842395512065087be38fb
少彬 <35789393+lazy-mapping@users.noreply.github.com>
2019-09-18 13:42:20 +0800

    Update TestJFrame.java
    测试在服务器修改数据，使用git在eclipse中拉取更新。

--- 测试/src/Swing/TestJFrame.java
+++ 测试/src/Swing/TestJFrame.java
@@ -1,12 +1,13 @@
 package Swing;
 
 import javax.swing.JFrame;
-/*GUI 创建一个空白窗口*/
+/*GUI 创建一个空白窗口，后来测试git，修改窗口大小300->500。*/
 public class TestJFrame {
 	public static void main(String[] args) {
 		JFrame f=new JFrame();
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setSize(300,300);
+		//f.setSize(300,300);
+		f.setSize(500,500);
 		f.setVisible(true);
 	}
 }
